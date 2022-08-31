ad725f77b80cf45dd6b6f61c504111ab2aafe7af
phuclk <phuclkcarryonto04@gmail.com>
2022-08-31 07:40:52 +0700

    add circleci config

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,3 @@
-echo ${KUBERNETES_CA} | base64 --decode > udagram-ca.pem
-echo ${KUBERNETES_CLIENT_CA} | base64 --decode > udagram-client-ca.pem
-echo ${KUBERNETES_CLIENT_KEY} | base64 --decode > udagram-key.pem
-kubectl config set-cluster udagram --server=${KUBERNETES_ENDPOINT} --certificate-authority=udagram-ca.pem
-kubectl config set-credentials -admin --client-certificate=udagram-client-ca.pem --client-key=udagram-key.pem
-kubectl config set-context -admin@udagram --cluster=udagram --namespace=default --user=admin
-kubectl config use-context -admin@udagram
-kubectl rollout restart deployment frond-end
+# Replace the [udagram-test01] below with the S3 bucket name
+aws s3 cp --recursive --acl public-read ./build s3://imagefiltering435567672677/
+# The trailing / at the end of the URL is important. 
